61d2daf1822a42361e8cdf559d3a3d1eea729508
@neobliviscaris2 <ergasiamellow@gmail.com>
2023-01-03 15:04:01 +0545

    fixed footer

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; 2022 <a href="https://aashatech.com/">AashaTech Pvt. Ltd</a>.</strong>
+    <strong>Copyright &copy; 2022 <a href="https://aashatech.com/" target="_blank">AashaTech Pvt. Ltd</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <a href="https://aashatech.com/">AashaTech Pvt. Ltd</a>
+        <a href="https://aashatech.com/" target="_blank">AashaTech Pvt. Ltd</a>
     </div>
 </footer>
