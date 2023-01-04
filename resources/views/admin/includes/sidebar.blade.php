11b947c9d18811f7e5e1da3e2ba523d2a966951b
@neobliviscaris2 <ergasiamellow@gmail.com>
2023-01-04 10:21:42 +0545

    gave routes

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ route('admin.index') }}" class="brand-link">
         <img src="{{ asset('css/adminasset/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">AdminLTE 3</span>
@@ -16,7 +16,7 @@
                     alt="User Image">
             </div>
             <div class="info">
-                <a href="#" class="d-block">Admin</a>
+                <a href="{{ route('admin.index') }}" class="d-block">Admin</a>
             </div>
         </div>
 
